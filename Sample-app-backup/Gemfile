762b4331bbcc2dfb786e18ce685191df40a22774
trovsankar <kalai.b@trov.co.in>
2016-06-28 11:36:43 +0530

    Initial commit

---
+++ Sample-app-backup/Gemfile
@@ -0,0 +1,24 @@
+#require "bundler/capistrano"
+source 'https://rubygems.org'
+
+group :development do
+  gem 'capistrano', '~> 3.0'
+  gem 'capistrano-npm'
+  gem 'capistrano-github'
+  gem 'capistrano-ssh-doctor'
+  gem 'capistrano-bundler'
+  gem 'capistrano-console'
+  gem 'capistrano-passenger', '>= 0.1.1'
+  gem 'capistrano-puma'
+  # Remove the following if your app does not use Rails
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
+  # Remove the following if your server does not use RVM
+ gem 'capistrano-rvm'
+ #gem 'capistrano-setup'
+ gem "capistrano-deploy"
+ gem 'unicorn'
+# gem 'capistrano-github', github: 'capistrano/github'
+# gem 'octokit', github: 'octokit/octokit.rb', branch: 'deployments-preview'
+
+end
---
+++ Sample-app-backup/config/deploy.rb
@@ -0,0 +1,92 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+
+set :repo_url, 'https://github.com/capistrano/rails.git'
+
+
+# If the environment differs from the stage name
+set :rails_env, 'staging'
+
+# Defaults to 'db'
+set :migration_role, 'migrator'
+
+# Defaults to false
+# Skip migration if files in db/migrate were not modified
+set :conditionally_migrate, true
+
+# Defaults to [:web]
+set :assets_roles, [:web, :app]
+
+# Defaults to 'assets'
+# This should match config.assets.prefix in your rails config/application.rb
+set :assets_prefix, 'prepackaged-assets'
+
+# If you need to touch public/images, public/javascripts, and public/stylesheets on each deploy
+set :normalize_asset_timestamps, %w{public/images public/javascripts public/stylesheets}
+
+# Defaults to nil (no asset cleanup is performed)
+# If you use Rails 4+ and you'd like to clean up old assets after each deploy,
+# set this to the number of versions to keep
+set :keep_assets, 2
+
+# Defaults to the primary :db server
+set :migration_role, :db
+set :migration_servers, -> { primary(fetch(:migration_role)) }
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+#set :application, 'my_app_name'
+#set :repo_url, 'https://github.com/trovsankar/sample-app.git'
+
+#set :github_access_token, '31ac4f7d6c2b8f3a5f90e4369d06745e89c3dc59'
+
+#set :database_name, ask('Enter the database name:')
+#ask(:database_encoding, 'UTF-8')
+
+#fetch(:database_encoding)
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
